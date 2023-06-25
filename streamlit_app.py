25751aaa7e9d7db716f1e6adcbe1f7b4b71a5a7a
Tukpetov <136899505+Tukpetov@users.noreply.github.com>
2023-06-25 11:48:00 +0100

    Update streamlit_app.py
    More deatiled description of breakfast food

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My healthy dinner')
+streamlit.title('My new healthy dinner')
+streamlit.header ('Breakfast menu')
+streamlit.text ('Eggs')
+streamlit.text ('Veggies')
+streamlit.text ('Water')
