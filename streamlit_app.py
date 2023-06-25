7c1eade0037505b34b4450d312e904d51e01f0b1
Tukpetov <136899505+Tukpetov@users.noreply.github.com>
2023-06-25 11:57:30 +0100

    Update streamlit_app.py
    changed menu and added emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title('My healthy dinner')
 streamlit.title('My new healthy dinner')
-streamlit.header ('Breakfast menu')
-streamlit.text ('Eggs')
-streamlit.text ('Veggies')
-streamlit.text ('Water')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
