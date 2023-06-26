4330b0cf01395d419adabd745080eeeb541fab9c
Tukpetov <136899505+Tukpetov@users.noreply.github.com>
2023-06-26 12:52:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-import urllib.error import URLError
+from urllib.error import URLError
 streamlit.title('My healthy dinner')
 streamlit.title('My new healthy dinner')
 streamlit.header('Breakfast Menu')
